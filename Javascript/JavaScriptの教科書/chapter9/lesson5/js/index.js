13f76101b9d50cead313f1a22021c09d0e1c062f
reiko.hojo <tama.toma.piyopiyo@gmail.com>
2019-09-20 12:50:23 +0900

    Javascript_jQueryのイベント

---
+++ Javascript/JavaScriptの教科書/chapter9/lesson5/js/index.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("#nice").click(function() {
+    alert("いいね！");
+  });
+  // 複数のイベントをまとめて設定
+  // $("#nice").on("mouseover mouseout", function() {
+  //   console.log("いいね！");
+  // });
+});
